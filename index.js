61cfa9a226956fff7fcfc01ced6feffede1e75ce
Brandon-Harris <brandonharris177@gmail.com>
2019-10-14 11:56:24 -0600

    function commit

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ server.get('/', (req, res) => {
 
 server.post('/api/register', (req, res) => {
   let user = req.body;
-
+  // validate the user
+  // hash the password
+  const hash = bcrypt.hashSync(user.password, 8);
+  // we override the password with the hash
+  user.password = hash;
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -30,22 +34,25 @@ server.post('/api/register', (req, res) => {
 
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
-
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'You cannot pass!!' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(400).json({ message: 'please provide credentials' });
+  }
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -54,11 +61,17 @@ server.get('/api/users', (req, res) => {
 });
 
 server.get('/hash', (req, res) => {
-  bcrypt.genSalt(10, function(err, salt) {
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync("B4c0/\/", salt);
+  const password = req.headers.authorization;
+  const hash = bcrypt.hashSync(password, 8);
+  res.status(200).json({hash})
 });
-});
+
+
+// implement the protected middleware that will check for username and password
+// in the headers and if valid provide access to the endpoint
+function protected() {
+  
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
