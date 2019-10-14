abfb564630dde4835f064aeb74a09142422fb453
Brandon-Harris <brandonharris177@gmail.com>
2019-10-14 10:59:04 -0600

    hashing not working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 const Users = require('./users/users-model.js');
@@ -50,6 +51,13 @@ server.get('/api/users', (req, res) => {
       res.json(users);
     })
     .catch(err => res.send(err));
+});
+
+server.get('/hash', (req, res) => {
+  bcrypt.genSalt(10, function(err, salt) {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync("B4c0/\/", salt);
+});
 });
 
 const port = process.env.PORT || 5000;
